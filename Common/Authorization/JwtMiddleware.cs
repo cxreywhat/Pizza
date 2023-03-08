44bcd34cde4b9befe16c558558b9a5977cbac5ce
cxreywhat <vl.loginov04@gmail.com>
2023-03-08 20:07:51 +0300

    commit

--- Common/Authorization/JwtMiddleware.cs
+++ Common/Authorization/JwtMiddleware.cs
@@ -1,10 +1,3 @@
-using System.Diagnostics;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.Extensions.Options;
-
 namespace Pizza.Common.Authorization
 {
     public class JwtMiddleware
@@ -19,9 +12,7 @@ namespace Pizza.Common.Authorization
         public async Task Invoke(HttpContext context, IUserRepository repository, IJwtUtils utils)
         {
             var token = context.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
-            Console.WriteLine(token);
             var userId = utils.ValidateJwtToken(token);
-            Console.WriteLine(userId);
             if (userId != null)
             {
                 var response = await repository.GetByIdAsync(userId.Value);
